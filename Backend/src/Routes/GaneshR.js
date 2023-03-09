2004b9755dc0fa66ca65baf56efb0fed1af574e0
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2023-03-09 13:46:31 +0800

    Update GaneshR.js

--- Backend/src/Routes/GaneshR.js
+++ Backend/src/Routes/GaneshR.js
@@ -1,2 +1,11 @@
+const express = require('Express');
+const router = express.Router();
+const controller = require('../Routes/GaneshR');
+
+//Products
+router.get('/product', controller,getProducts);
+router.get('/product/:product_id', controller.getProductByID);
+
+
 
 
