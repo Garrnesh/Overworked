f9bc46db984c4f69a741f53778940ca6e614e305
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2023-03-18 01:08:13 +0800

    Editted server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = require("./Routes/GaneshR")
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
