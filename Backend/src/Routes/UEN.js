2483a020a85ef39bd27f659d80feb1f03c0757fb
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2023-03-18 23:02:11 +0800

    Added UEN number feature

---
+++ Backend/src/Routes/UEN.js
@@ -0,0 +1,31 @@
+const express = require('Express');
+const UEN_router = express.Router();
+const axios = require('axios');
+
+UEN_router.get('/UEN/:UEN', async (req,res,) => {
+
+    const UEN = req.params.UEN;
+
+    const params = {
+        resource_id: '5ab68aac-91f6-4f39-9b21-698610bdf3f7',
+    };
+
+    const UEN_url = 'https://data.gov.sg/api/action/datastore_search' + '?resource_id=' + params.resource_id + '&q=' + UEN;
+
+    const verify_send = {}
+
+    try {
+        const verify = await axios.get(UEN_url);
+        const verify_JSON = verify.data;
+
+        if (verify_JSON.result.total == 1) {
+            res.status(200).json(verify_JSON["result"]["records"]);
+        } else {
+            res.status(404).send("Business not found");
+        }
+    } catch(err){
+        res.status(500).send(err.message);
+    }
+});
+
+module.exports = UEN_router;
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const router = require("./Routes/GaneshR")
+const router = require("./Routes/GaneshR");
+const UEN_router = require("./Routes/UEN");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use('/api', router);
+app.use('/api', UEN_router);
+
 
 // app.use('/', (req,res,next) => {
 //     res.status(404).json({
