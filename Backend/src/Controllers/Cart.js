24f46bfc8b1ce82469ffa3c7ee3a6df478b8304c
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2023-03-21 20:32:39 +0800

    Added cart

---
+++ Backend/src/Controllers/Cart.js
@@ -0,0 +1,154 @@
+//Remember to include this in routes later
+const { FieldValue } = require("firebase-admin/firestore");
+const { Carts, Cartitems } = require("../firebase.js");
+
+//Cart table
+const getCart = async (req,res) => {
+    try{
+        const cart = await Carts.get();
+        res.status(200).json(cart.data());
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+}
+
+const getCartByID = async (cart_id) => {
+    const cart = await Carts.doc(cart_id).get();
+    if(cart.exists){
+        return cart;
+    }else{
+        throw new Error("Cart does not exist");
+    }
+};
+
+const checkCartID = async (cart_id) => {
+    try{
+        const cart = await Carts.doc(cart_id).get();
+        if(cart.exists){
+            throw new Error("Cart with cart id already exists");
+        }
+    }catch(err){
+        throw new Error("Cart with cart id already exists");
+    }
+};
+
+const addCart = async (cart_id, buyer_id, status) => {
+    const cart = Carts.doc(cart_id);
+    try{
+        await cart.set({
+            buyer_id: buyer_id,
+            status: status});
+    }catch(err){
+        throw new Error("Unable to create new cart");
+    }
+};
+
+const removeCart = async (cart_id) => {
+    try{
+        await Orders.doc(order_id).delete();
+    }catch(err){
+        throw new Error("Unable to delete order");
+    }
+};
+
+const getCartByBuyerID = async(buyer_id) => {
+    const cart = await Carts.where('buyer_Id', '==', buyer_id).get();
+    if(cart.size>0){
+        return cart;
+    }else{
+        throw new Error("No cart with stated buyer ID");
+    }
+}
+
+const removeCartByBuyerID = async(buyer_id) => {
+    try{
+        await Orders.where("buyer_id", "==", buyer_id).delete();
+    }catch(err){
+        throw new Error("Unable to delete cart");
+    }
+}
+
+//Cartitem table
+const getCartItems = async (req,res) => {
+    try{
+        const cartitem = await Cartitems.get();
+        res.status(200).json(cartitem.data());
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+}
+
+const getCartItemByID = async (cartitem_id) => {
+    const cartitem = await Cartitems.doc(cartitem_id).get();
+    if(cartitem.exists){
+        return cartitem;
+    }else{
+        throw new Error("Cart Item does not exist");
+    }
+};
+
+const checkCartItemID = async (cartitem_id) => {
+    try{
+        const cartitem = await Cartitems.doc(cartitem_id).get();
+        if(cartitem.exists){
+            throw new Error("Cart item with Cart Item ID already exists");
+        }
+    }catch(err){
+        throw new Error("Cart item with Cart Item ID already exists")
+    }
+}
+
+const addCartItem = async (cartitem_id, cart_id, product_id, quantity) => {
+    const cartitem = Cartitems.doc(cartitem_id);
+    try{
+        await cartitem.set({
+            cart_id: cart_id, 
+            product_id: product_id,
+            quantity: quantity
+        });
+    }catch(err){
+        throw new Error("Unable to create new Cart item");
+    }
+};
+
+const removeCartItem = async (cartitem_id) => {
+    try{
+        await Cartitems.doc(cartitem_id).delete();
+    }catch(err){
+        throw new Error("Unable to delete cart item");
+    }
+};
+
+const getCartItemByCartId = async(cart_id) => {
+    const cartitem = await Cartitems.where('cart_id', '==', cart_id).get();
+    if(cartitem.size>0){
+        return cartitem;
+    }else{
+        throw new Error("No order item with stated Cart item ID");
+    }
+}
+
+const removeCartItemByCartID = async (cart_id) => {
+    try{
+        await Cartitems.where("cart_id", "==", cart_id).delete();
+    }catch(err){
+        throw new Error("Unable to delete cart item with specified cart ID")
+    }
+}
+
+module.exports = {
+    getCart,
+    getCartByID,
+    checkCartID,
+    addCart,
+    removeCart,
+    getCartByBuyerID,
+    removeCartByBuyerID,
+    getCartItems,
+    getCartItemByID,
+    checkCartItemID,
+    addCartItem,
+    removeCartItem,
+    getCartItemByCartId,
+    removeCartItemByCartID,
+}
