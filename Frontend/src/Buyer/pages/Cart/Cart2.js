41bf9d6f93f7d023b770232912c991722acedbdc
Delaney Quek <delaneyquek@gmail.com>
2023-04-03 17:27:04 +0800

    updated on 3/4

--- Frontend/src/Buyer/pages/Cart/Cart2.js
+++ Frontend/src/Buyer/pages/Cart/Cart2.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../../../useFetch";
 import Image from 'react-bootstrap/Image';
@@ -8,7 +8,7 @@ import { BsFillTrashFill } from "react-icons/bs";
 import { Button, Container, Nav } from "react-bootstrap";
 // import PortalNavbar from "../../Navbar";
 const Cart = () => {
-    const [name, setName] = useState("Sally");
+    const [name, setName] = useState("Teddy");
     const { data: cartitems, isPending, error } = useFetch('http://localhost:8000/cartitems/cart_id/' + name);
     const navigate = useNavigate();
     let count = 0;
@@ -16,83 +16,91 @@ const Cart = () => {
         count++;
     }
     // console.log(count);
-    const handleAdd = (id, cpid, qty, pid) => {
+    const handleAdd = (id, qty) => {
+        console.log(id);
         if (qty < 5) {
-            const itemUrl = 'http://localhost:8000/cartitems/' + id;
             const replaced = {
-                "cart_id": cpid,
-                "quantity": parseFloat(qty) - 1,
-                "product_id": pid
+                "cartitem_id": id,
+                "quantity": parseFloat(qty) +  1
+                
             };
-            fetch('http://localhost:8000/cart/' + id, {
+            fetch('http://localhost:8000/cartitems/' + id, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(replaced)
             })
+            window.location.reload(false);
+        }
+        else{
+            alert("Max Quantity is 5")
         }
     }
 
-    const handleMinus = (id, cpid, qty, pid) => {
-        const replaced = {
-            "cart_id": cpid,
-            "quantity": parseFloat(qty) + 1,
-            "product_id": pid
-        };
-        fetch('http://localhost:8000/cartitems/' + id, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(replaced)
-        })
+    const handleMinus = (id, qty, product_id) => {
+        console.log(id, qty, product_id);
+        if (qty > 1) {
+            const replaced = {
+                "cartitem_id": id,
+                "quantity": parseFloat(qty) -  1
+                
+            };
+            fetch('http://localhost:8000/cartitems/' + id, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(replaced)
+            })
+            window.location.reload(false);
+        }
+        else{
+            alert("Press Delete Button to remove item from Cart")
+        }
     }
 
-
-
     const handleClick = (id) => {
-        fetch('http://localhost:8000/cartitems/' + name, {
+        
+        fetch('http://localhost:8000/cartitems/' + id, {
             method: 'DELETE'
         })
         window.location.reload(false);
     }
-    const [productItems, setProductItems] = useState({});
-    const [image, setImage] = useState("");
-    const [productName, setProductName] = useState("");
-    const [productPrice, setProductPrice] = useState("");
-    const [productSize, setProductSize] = useState("");
-    //get product image, product name, product price 
-    async function getInfo(id) {
-        try {
-            const response = await fetch("http://localhost:8000/products/" + id, {
-                method: 'GET',
-            });
-            const response_json = await response.json();
+    //state variable to store the products to be displayed
 
-            setImage(response_json["product_image"]);
-            setProductName(response_json["listing_name"]);
-            setProductPrice(response_json["product_price"]);
-            setProductSize(response_json["product_size"]);
-            // console.log(image, productName, productPrice, productSize);
+    const [productItems, setProductItems] = useState([]);
 
-        } catch (err) {
-            console.log(err);
-        }
+    async function addProductToCart(item) {
+      try {
+        const response = await fetch("http://localhost:8000/products/" + item.product_id, {
+          method: "GET"
+        });
+        const productData = await response.json();
+        const newProductItem = {
+          Pid :  item.product_id,
+          Pimage: productData.product_image,
+          Productname: productData.listing_name,
+          Productprice: productData.product_price,
+          Productsize: productData.product_size,
+          cart_id: item.id,
+          quantity: item.quantity
+        };
+        setProductItems(prevItems => [...prevItems, newProductItem]);
+      } catch (err) {
+        console.log(err);
+      }
     }
     
-        cartitems && cartitems.map(item => {
-            const productInfo = getInfo(item.product_id);
-            const newItem = {
-                Pimage: image,
-                Productname: productName,
-                Productprice: productPrice,
-                Productsize: productSize
-            }
-
-            setProductItems([...productItems, newItem]);
-        })
-
+    useEffect(() => {
+      if (cartitems) {
+        cartitems.forEach(item => {
+          addProductToCart(item);
+        });
+      }
+    }, [cartitems]);
+    
+    console.log(productItems);
 
 
 
@@ -114,32 +122,32 @@ const Cart = () => {
                                     <div class="row d-flex justify-content-between align-items-center">
                                         {/* <!-- add image of picture--> */}
                                         <div class="col-md-2 col-lg-2 col-xl-2 m-lg-0">
-                                            <img className="img-fluid rounded-3" style={{ height: "200px", objectFit: 'contain' }} src={item.Image} />
+                                            <img className="img-fluid rounded-3" style={{ height: "200px", objectFit: 'contain' }} src={item.Pimage} />
                                         </div>
                                         {/* <!-- name of the shirt, size and color--> */}
                                         <div class="col-md-3 col-lg-3 col-xl-3">
                                             <p class="lead fw-bold mb-2">{item.Productname}</p>
                                             <p><span class="text-muted">Size: </span>{item.Productsize}</p>
-                                            <h5 class="mb-0">${item.Productprice}</h5>
+                                            <p><span class="text-muted">$</span>{item.Productprice}</p>
                                         </div>
                                         {/* <!-- this for the quantity--> */}
                                         <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
                                             {/* <p class="mb-2 fw-normal">Qty:</p> */}
 
-                                            <button class="btn btn-outline-light text-dark" type="button" onClick={() => handleMinus(item.id, item.cart_id, item.quantity, item.product_id)}>-</button>
+                                            <button class="btn btn-outline-light text-dark" type="button" onClick={() => handleMinus(item.cart_id, item.quantity)}>-</button>
 
                                             <input type="text" class="form-control text-center" style={{
                                                 width: "45px"
                                             }} value={parseFloat(item.quantity)} />
                                             <div class="input-group-prepend">
-                                                <button class="btn btn-outline-light text-dark" type="button" onClick={() => handleAdd(item.id, item.cart_id, item.quantity, item.product_id)}>+</button>
+                                                <button class="btn btn-outline-light text-dark" type="button" onClick={() => handleAdd(item.cart_id, item.quantity)}>+</button>
                                                 {/* <!-- this all for the quantity--> */}
                                             </div>
                                         </div>
 
                                         {/* <!-- button to delete the order--> */}
                                         <div class="col-md-1 col-lg-1 col-xl-1">
-                                            <Button onClick={() => { handleClick(item.product_id) }} variant="light">
+                                            <Button onClick={() => { handleClick(item.cart_id) }} variant="light">
                                                 <BsFillTrashFill size={24} />
                                             </Button>
                                         </div>
@@ -148,71 +156,10 @@ const Cart = () => {
                             </div>
                         </div>)
                 })}
-            <Link to="/checkout" className="btn btn-secondary btn-dark btn-block btn-lg col-12">Add To Checkout
+            <Link to= {{ pathname: "/checkout", state : {productItems}}} className="btn btn-secondary btn-dark btn-block btn-lg col-12">Add To Checkout
             </Link>
         </div>
     )
-
-    // <div className="cartitems container-xxl">
-
-    //     {/* {error && <div>{error}</div>}
-    //             {isPending && <div>Loading...</div>} */}
-
-    //     {/* {count === 0 &&
-    //         <div className="text-center mb-2 mt-2 fw-light">
-    //             <h1>Cart</h1>
-    //             <h5 className="fw-bold">Items in Cart will be checked out</h5>
-    //             <h6>Cart is empty. Add to Cart to see your items here!</h6>
-    //         </div>
-    //     } */}
-    // {
-    //     cartitems && cartitems.map(item => {
-    //         const productInfo = getInfo(item.product_id); 
-    //         return (
-    //             <div className="container-sm">
-
-    //                 <div class="card rounded-3 mb-4">
-    //                     <div class="card-body p-4">
-    //                         <div class="row d-flex justify-content-between align-items-center">
-    //                             {/* <!-- add image of picture--> */}
-    //                             <div class="col-md-2 col-lg-2 col-xl-2 m-lg-0">
-    //                                 <img className="img-fluid rounded-3" style={{ height: "200px", objectFit: 'contain' }} src={image} />
-    //                             </div>
-    //                             {/* <!-- name of the shirt, size and color--> */}
-    //                             <div class="col-md-3 col-lg-3 col-xl-3">
-    //                                 <p class="lead fw-bold mb-2">{productName}</p>
-    //                                 <p><span class="text-muted">Size: </span>{productSize}</p>
-    //                                 <h5 class="mb-0">${productPrice}</h5>
-    //                             </div>
-    //                             {/* <!-- this for the quantity--> */}
-    //                             <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-    //                                 {/* <p class="mb-2 fw-normal">Qty:</p> */}
-
-    //                                 <button class="btn btn-outline-light text-dark" type="button" onClick={() => handleMinus(item.id, item.cart_id, item.quantity, item.product_id)}>-</button>
-
-    //                                 <input type="text" class="form-control text-center" style={{
-    //                                     width: "45px"
-    //                                 }} value={parseFloat(item.quantity)} />
-    //                                 <div class="input-group-prepend">
-    //                                     <button class="btn btn-outline-light text-dark" type="button" onClick={() => handleAdd(item.id, item.cart_id, item.quantity, item.product_id)}>+</button>
-    //                                     {/* <!-- this all for the quantity--> */}
-    //                                 </div>
-    //                             </div>
-
-    //                             {/* <!-- button to delete the order--> */}
-    //                             <div class="col-md-1 col-lg-1 col-xl-1">
-    //                                 <Button onClick={() => { handleClick(item.product_id) }} variant="light">
-    //                                     <BsFillTrashFill size={24} />
-    //                                 </Button>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>)
-    //     })}
-    //     <Link to="/checkout" className="btn btn-secondary btn-dark btn-block btn-lg col-12">Add To Checkout
-    //     </Link>
-    // </div>
 
 }
 
--- Frontend/src/Buyer/pages/Cart/Checkout_Details.js
+++ Frontend/src/Buyer/pages/Cart/Checkout_Details.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState , useEffect} from "react";
 // import logo from './thriftitlogo.png'
 import { PersonCircle } from "react-bootstrap-icons";
 import { Telephone } from "react-bootstrap-icons";
@@ -11,63 +11,93 @@ import { GlobeAmericas } from "react-bootstrap-icons";
 import useFetch from "../../../useFetch";
 
 
-const CheckoutPage = () => {
+
+const CheckoutPage = (props) => {
+    const {productItemss} = props; 
     const navigate = useNavigate();
 
-    const { data: addresses, error, isPending } = useFetch('http://localhost:8000/addresses');
+    const { data: addresses, error, isPending } = useFetch('http://localhost:8000/address');
     const { data: payments } = useFetch('http://localhost:8000/payments');
-    const { data: orders } = useFetch('http://localhost:8000/cart');
+
+    const [name, setName] = useState("Teddy");
+    const { data: orderitems} = useFetch('http://localhost:8000/cartitems/cart_id/' + name);
+
     const [totalPrice, setTotalPrice] = useState(0);
-    const [chosenaddress, setChosenaddress] = useState({});
-    const [chosenpayment, setChosenpayment] = useState({});
-    
-    const total = orders ? orders.reduce((acc, item) => acc + parseFloat(item.cartproduct_price)*parseFloat(item.cartproduct_quantity), 0) : 0;
-    
-    //choose payment 
-    const handlePayment = (cardNumber, nameCard, expiryDate, cvv) => {
-        const payment = {
-            "cardNumber": cardNumber,
-            "nameCard": nameCard,
-            "expiryDate": expiryDate,
-            "CVV": cvv
-        };
-        setChosenpayment(payment);
-        console.log(payment);
-    }
-
-    // const total = orders.reduce((price, order) => price + parseFloat(order.cartproduct_price), 0);
-
-   
-    //choose address
-    const handleAddress = (pincode, txtaddress, locality, city) => {
-        const address = {
-            "pincode": pincode,
-            "txtaddress": txtaddress,
-            "locality": locality,
-            "city": city
-        };
-        setChosenaddress(address);
-        console.log(address);
-    }
-    //when button is pressed 
-    const handleConfirm = () => {
+    // const [chosenaddress, setChosenaddress] = useState("");
+    // const [chosenpayment, setChosenpayment] = useState("");
         
-        const confirmation = {
-            "orders": orders,
-            "totalPrice": 300,
-            "address": chosenaddress,
-            "payment": chosenpayment
+   const [orderAddress, setorderAddress] = useState('');
+   const [card, setCard] = useState('');
+
+    // const handleConfirm = () => {
+        
+    //     const confirmation = {
+    //         // "orders": orders,
+    //         // "totalPrice": total,
+    //         "address": chosenaddress,
+    //         "payment": chosenpayment
+    //     }
+    //     console.log(confirmation)
+    //     fetch('http://localhost:8000/confirmation/', {
+    //         method: 'POST',
+    //         headers: { "Content-Type": "application/json" },
+    //         body: JSON.stringify(confirmation)
+    //     }).then(() => {
+    //         navigate(-1);
+    //         window.location.reload(false);
+    //     })
+    // }
+    //to mask the credit card details for safety
+    const maskNumber = (number) => {
+        const lastFourDigits = number.slice(-4);
+        const maskedNumber = "*".repeat(number.length - 4) + lastFourDigits;
+        return maskedNumber;
+      };
+
+      //show order details
+      const [productItems, setProductItems] = useState([]);
+
+      async function addProductToCart(item) {
+        try {
+          const response = await fetch("http://localhost:8000/products/" + item.product_id, {
+            method: "GET"
+          });
+          const productData = await response.json();
+          const newProductItem = {
+            Pid :  item.product_id,
+            Pimage: productData.product_image,
+            Productname: productData.listing_name,
+            Productprice: productData.product_price,
+            Productsize: productData.product_size,
+            cart_id: item.id,
+            quantity: item.quantity
+          };
+          setProductItems(prevItems => [...prevItems, newProductItem]);
+        } catch (err) {
+          console.log(err);
         }
-        console.log(confirmation)
-        fetch('http://localhost:8000/confirmation/', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(confirmation)
-        }).then(() => {
-            navigate(-1);
-            window.location.reload(false);
-        })
-    }
+      }
+      const total = productItems ? productItems.reduce((acc, item) => acc + parseFloat(item.Productprice)*parseFloat(item.quantity), 0) : 0;
+
+      const handleAddress =(id) =>{
+        setorderAddress(id);
+      }
+
+      const handlePayment =(id) =>{
+        setCard(id);
+      }
+
+      console.log(orderAddress, card)
+      
+      useEffect(() => {
+        if (orderitems) {
+          orderitems.forEach(item => {
+            addProductToCart(item);
+          });
+        }
+      }, [orderitems]);
+      
+      console.log(productItems);
 
     return (
         <div className="address-list p-5 mb-5 container-sm">
@@ -91,23 +121,26 @@ const CheckoutPage = () => {
                         <form>
                             {addresses && addresses.map(address => (
                                 
-                                <div className="address-list" key={address.id}>
+                                <div className="address-list">
                                     <div className="container mb-3">
                                         <div className="card border">
                                             <div className="row">
                                                 <div class="col-2">
 
-                                                    <label class="btn py-4 mt-5" for="address">
-                                                        <input name="address" type="radio" onClick={() => handleAddress(address.pincode, address.txtaddress, address.locality, address.city)} />
+                                                    <label class="btn py-4 mt-2" for="address">
+                                                        <input name="address" type="radio" onClick={() => handleAddress(address.address_id)} />
+                                                        {/* <input name="address" type="radio"/> */}
+
                                                     </label>
 
                                                 </div>
                                                 <div className="card-body text-start py-4 col-10">
-                                                    <h5 className="card-title">{address.Name}</h5>
-                                                    <p className="card-text m-0">{address.txtaddress}</p>
-                                                    <p className="card-text m-0">{address.pincode}</p>
-                                                    <p className="card-text m-0">{address.city}</p>
-                                                    <p className="card-text">Phone Number: {address.phoneNumber}</p>
+                                                    {/* <h5 className="card-title">{address.Name}</h5> */}
+                                                    <p className="card-text m-0">{address.id}</p>
+                                                    <p className="card-text m-0">{address.address}</p>
+                                                    <p className="card-text m-0">{address.postal_code}</p>
+                                                    {/* <p className="card-text m-0">{address.city}</p> */}
+                                                    {/* <p className="card-text">Phone Number: {address.phoneNumber}</p> */}
                                                 </div>
                                             </div>
 
@@ -127,18 +160,20 @@ const CheckoutPage = () => {
                         </div>
                         <form>
                             {payments && payments.map(payment => (
-                                <div className="address-list" key={payment.id}>
+                                <div className="address-list">
                                     <div className="container mb-3">
                                         <div className="card border">
                                             <div className="row">
                                                 <div class="col-2">
                                                     <label class="btn py-4 mt-3" for="address">
-                                                        <input name="address" type="radio" onClick={() => handlePayment(payment.cardNumber, payment.nameCard, payment.expiryDate, payment.CVV)} />
+                                                        <input name="address" type="radio" onClick={() => handlePayment(payment.payment_id)} />
+
                                                     </label>
                                                 </div>
                                                 <div className="card-body text-start py-4 col-10">
-                                                    <h5 className="card-title">{payment.cardNumber}</h5>
-                                                    <p className="card-text m-0">{payment.nameCard}</p>
+                                                    <h5 className="card-title">{payment.payment_id}</h5>
+                                                    <h5 className="card-title">{payment.name_on_card}</h5>
+                                                    <p className="card-text m-0">{maskNumber(payment.card_number)}</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -154,16 +189,15 @@ const CheckoutPage = () => {
                                 Order Summary
                             </h4>
 
-                            {/* choose payment */}
-                            {orders && orders.map(order => (
+                            {/* order details */}
+                            {productItems && productItems.map(order => (
 
-                                <div className="address-list" key={order.id}>
-                                    {/* <div>{totalPrice} += parseFloat({order.cartproduct_price})*parseFloat({order.cartproduct_quantity})</div> */}
+                                <div className="address-list">
                                     <div className="container mb-3">
                                         <div className="row">
-                                            <div className="col-sm-6">{order.cartproduct_name}</div>
-                                            <div className="col-sm-3">Qty: {order.cartproduct_quantity}</div>
-                                            <div className="col-sm-3">${order.cartproduct_price}</div>
+                                            <div className="col-sm-6">{order.Productname}</div>
+                                            <div className="col-sm-3">Qty: {order.quantity}</div>
+                                            <div className="col-sm-3">${order.Productprice}</div>
                                         </div>
                                     </div>
                                 </div>
@@ -174,14 +208,14 @@ const CheckoutPage = () => {
                                 <div className="row">
                                     <div className="col-sm-9 fw-bold">Total Price:</div>
 
-                                    <div className="col-sm-3">${total}</div>
+                                    <div className="col-sm-3">${total.toFixed(2)}</div>
                                 </div>
                             </div>
 
                         </div>
-                        {/* <button type="checkout" class="btn btn-secondary btn-dark btn-block btn-lg col-12" onClick={handleConfirm}>Proceed to Order</button> */}
-                        <Link to="/confirmationpage" className="btn btn-secondary btn-dark btn-block btn-lg col-12" onClick={handleConfirm}>Proceed to Order
-                        </Link>
+                        <button type="checkout" class="btn btn-secondary btn-dark btn-block btn-lg col-12">Proceed to Order</button>
+                        {/* <Link to="/confirmationpage" className="btn btn-secondary btn-dark btn-block btn-lg col-12" onClick={handleConfirm}>Proceed to Order
+                        </Link> */}
                     </div>
                 </div>
             </div>
