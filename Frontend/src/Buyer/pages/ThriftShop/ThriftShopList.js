af9a97d85b2ad1ae64636777a8a52dc14fee2d59
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2023-04-03 13:12:48 +0800

    Able to sort by distance now

--- Frontend/src/Buyer/pages/ThriftShop/ThriftShopList.js
+++ Frontend/src/Buyer/pages/ThriftShop/ThriftShopList.js
@@ -39,8 +39,6 @@ const ThriftShopList = ({ thriftshops }) => {
       setError("Geolocation is not supported by this browser.");
     }
   }
-  console.log(latitude, longitude)
-  console.log(thriftshop)
 
   async function getRoute(username){
     const check = {
@@ -60,30 +58,35 @@ const ThriftShopList = ({ thriftshops }) => {
       return distance
     }
   }
-  // const [before_sort, setbefore_sort] = useState({})
-  // useEffect(() => {
-  //   if (latitude !== null && longitude !== null) {
-  //     for (let i=0; i<thriftshop.length; i++){
-  //       const username = thriftshop[i].id
-  //       setbefore_sort({...before_sort, username: getRoute(username)})
-  //     }
-  //     console.log(before_sort)
-  //   }
-  // }, [latitude, longitude, thriftshop])
 
-  const before_sort = {};
-  const sorting = [];
+  async function sortShop(){
+    const before_sort = {};
+    const thriftshopsorted = [];
+    getLocation();
+    console.log(latitude);
+    console.log(longitude);
 
-  if (latitude !== null && longitude !== null) {
-    for (let i=0; i<thriftshop.length; i++){
-      const username = thriftshop[i].id
-      before_sort[username] = getRoute(username)
-      sorting[i] = getRoute(username)
+    if (latitude !== null && longitude !== null) {
+      for (let i=0; i<thriftshop.length; i++){
+        const username = thriftshop[i].id
+        before_sort[username] = await getRoute(username)
+      }
+      const sortedArray = Object.entries(before_sort).sort((a, b) => a[1] - b[1]);
+      const sortedObj = Object.fromEntries(sortedArray); //This will sort the object
+      const sortedKeys = Object.keys(sortedObj); //This will return the sorted keys as an array
+      for (let j=0; j<sortedKeys.length; j++){
+        for(let k=0; k<thriftshop.length; k++){
+          if(sortedKeys[j] === thriftshop[k].id){
+            thriftshopsorted[j] = thriftshop[k]
+          }
+        }
+      }
+      setThriftShop(thriftshopsorted);
+      // setIsLoading(false);
     }
-    const sorted = sorting.sort()
-    console.log(sorted)
   }
   
+  sortShop();
 
   return (
     <>
@@ -96,7 +99,9 @@ const ThriftShopList = ({ thriftshops }) => {
             <h2 className="text">Filter Thrift Shops</h2>
             <button className="btn btn-outline-dark w-100 mb-4" onClick = {()=>filterResult('True')}>Donation Point</button>
             <button className="btn btn-outline-dark w-100 mb-4" onClick = {() => setThriftShop(thriftshops)}>All Thrift Shops</button>
-            <button className="btn btn-outline-dark w-100 mb-4" onClick={getLocation}>Sort by Location</button>
+            {/* <button className="btn btn-outline-dark w-100 mb-4" onClick={sortShop()}>Sort by Location</button> */}
+            <button className="btn btn-outline-dark w-100 mb-4" onClick={sortShop}>Sort by Location</button>
+
           </div>
 
           <div className="col-md-9">
